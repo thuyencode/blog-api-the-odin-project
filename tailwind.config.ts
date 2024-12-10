8f1beb6fca4ba2c6b8139176fc1ac85a07af4db6
Thuyen Code <thuyencode@proton.me>
2024-12-10 20:58:31 +0700

    Install Tailwind CSS, DaisyUI and Prettier

---
+++ tailwind.config.ts
@@ -0,0 +1,29 @@
+import daisyui from 'daisyui'
+import daisyuiThemes from 'daisyui/src/theming/themes'
+import type { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      container: { center: true },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...daisyuiThemes.winter
+        },
+        dark: {
+          ...daisyuiThemes.night
+        }
+      }
+    ]
+  }
+} satisfies Config
