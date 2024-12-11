ebe7efcb74707993a980b75838abd41e78df2bc0
Thuyen Code <thuyencode@proton.me>
2024-12-11 08:51:28 +0700

    Install many Express's middlewares

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,12 +1,25 @@
-import express from 'express'
+import compression from 'compression'
+import e from 'express'
+import helmet from 'helmet'
 import ViteExpress from 'vite-express'
 
-const app = express()
+const app = e()
+
+app.use(
+  helmet({
+    contentSecurityPolicy: false
+  })
+)
+
+app.use(e.json())
+app.use(e.urlencoded({ extended: true }))
+
+app.use(compression())
 
 app.get('/hello', (_, res) => {
   res.send('Hello Vite + React + TypeScript!')
 })
 
-ViteExpress.listen(app, 3000, () =>
+ViteExpress.listen(app, 3000, () => {
   console.log('Server is listening on port 3000...')
-)
+})
