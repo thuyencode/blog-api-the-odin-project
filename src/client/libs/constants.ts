c6aeaae786075d9d3a9b2c5883d709b8da4a7158
Thuyen Code <thuyencode@proton.me>
2024-12-16 21:58:21 +0700

    Constant values

---
+++ src/client/libs/constants.ts
@@ -0,0 +1,9 @@
+export const MUTATIONS_KEYS = {
+  LOG_IN: '/api/auth/log-in',
+  REGISTER: '/api/auth/register'
+}
+
+export const QUERY_KEYS = {
+  AUTH_STATUS: '/api/auth/status',
+  AUTH_REFRESH: '/api/auth/refresh'
+}
